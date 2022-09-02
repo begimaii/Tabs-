3ab2c21b07254bf265e4f5339efbf690aff3c5d2
Begimai Abdybekova <b.abdybekova@gmail.com>
2022-09-02 10:55:07 -0500

    tabs created

---
+++ src/tabs/Tabs.jsx
@@ -0,0 +1,69 @@
+
+import { Component } from "react";
+import {tabs} from "./data"
+import {styles} from "./styles"
+
+class Tabs extends Component{
+    constructor(){
+        super()
+        this.state = {
+            tabs:tabs,
+            selected: 0,
+        }
+    }
+    changeTab = (index) => {
+    //   const copyState = {...this.state}
+    //   copyState.selected = index
+        this.setState({...this.state, selected:index})
+        index.target.style.background = "white"
+        index.target.style.color = "teal"
+    }
+
+    
+render(){
+    const {tabs,selected} = this.state
+    const hoverHandler = (e) => {
+        e.target.style.background = "white"
+       
+        
+      }
+      const outHandler = (e) => {
+        e.target.style.background = "lightblue"
+      
+      }
+return(
+    <>
+    <h1 style = {styles}>Tabs</h1>
+    <div style = {styles.btn}>
+    {
+        tabs.map((tab,index) =>{
+            return(
+                <button onMouseOver = {hoverHandler} onMouseOut = {outHandler} style = {styles.button} key = {tab.id} onClick ={()=> this.changeTab(index) }>{tab.title}</button>
+            )
+        })
+    }
+    </div>
+    <div style = {styles.container}>
+       
+ <h3> {this.state.tabs[selected].title} </h3>
+        {this.state.tabs[selected].description}
+        {this.state.tabs[selected]?.options && (
+            <ul>
+                {this.state.tabs[selected].options.map(option=>{
+                    return (
+                        <li>{option}</li>
+                    )
+                })}
+            </ul>
+        )} 
+
+    </div>
+    </>
+)
+
+}
+}
+
+export default Tabs
+
+
---
+++ src/tabs/styles.js
@@ -0,0 +1,31 @@
+export const styles = {
+    btn : {
+        display : "flex",
+        
+    },
+    button : {
+        width : "254px",
+        height : "60px",
+        fontSize : "30px",
+        letterSpacing: "10px",
+        border : "none",
+        cursor : "pointer",
+        backgroundColor : "lightblue"
+
+    },
+    container : {
+        width : "700px",
+        backgroundColor : "white",
+        borderBottomLeftRadius: "15px",
+        borderBottomRightRadius: "15px",
+        padding : "20px 31px",
+        textAlign: "left",
+        fontSize : "20px",
+        color : "teal",
+    },
+    h1 : {
+        color : "black",
+     
+    },
+   
+}
